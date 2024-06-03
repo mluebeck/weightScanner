10bce3ef190112483dbd5df90508c38159d5fae8
Mario Rotz <mariorotz@googlemail.com>
2024-06-03 23:14:31 +0200

    add xcode project

---
+++ WeightScanner/WeightScanner/WeightScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightScannerApp.swift
+//  WeightScanner
+//
+//  Created by Mario Rotz on 03.06.24.
+//
+
+import SwiftUI
+
+@main
+struct WeightScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeightScanner/WeightScannerUITests/WeightScannerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WeightScannerUITestsLaunchTests.swift
+//  WeightScannerUITests
+//
+//  Created by Mario Rotz on 03.06.24.
+//
+
+import XCTest
+
+final class WeightScannerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
