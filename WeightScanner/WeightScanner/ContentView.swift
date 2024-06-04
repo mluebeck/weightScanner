fd9e45aef675b8f1efad196dc64144f546346cc5
Mario Rotz <mariorotz@googlemail.com>
2024-06-04 22:25:34 +0200

    adding image Picker

--- WeightScanner/WeightScanner/ContentView.swift
+++ WeightScanner/WeightScanner/ContentView.swift
@@ -1,21 +1,70 @@
-//
-//  ContentView.swift
-//  WeightScanner
-//
-//  Created by Mario Rotz on 03.06.24.
-//
-
 import SwiftUI
+import ImageCaptureCore
+import Vision
+import AVFoundation
 
 struct ContentView: View {
+    @State private var isShowingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var recognizedText: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let inputImage = inputImage {
+                Image(uiImage: inputImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+            } else {
+                Rectangle()
+                    .fill(Color.secondary)
+                    .frame(height: 300)
+            }
+            
+            Text(recognizedText)
+                .padding()
+            
+            Button("Take Photo") {
+                isShowingImagePicker = true
+            }
+            .padding()
         }
-        .padding()
+        .sheet(isPresented: $isShowingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        recognizeTextInImage(image: inputImage)
+    }
+    
+    func recognizeTextInImage(image: UIImage) {
+        guard let cgImage = image.cgImage else { return }
+        
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        let request = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            
+            var recognizedText = ""
+            for observation in observations {
+                guard let topCandidate = observation.topCandidates(1).first else { continue }
+                recognizedText += topCandidate.string + "\n"
+            }
+            
+            DispatchQueue.main.async {
+                self.recognizedText = recognizedText
+            }
+        }
+        
+        request.recognitionLevel = .accurate
+        request.usesLanguageCorrection = true
+        
+        do {
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the request: \(error)")
+        }
     }
 }
 
